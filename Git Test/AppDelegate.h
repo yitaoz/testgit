fe4de56c7041e46adc359a4f3fa856434251499f
Yitao Zhang <zhangyitao91@gmail.com>
2015-04-10 22:58:26 +0800

    Initial Commit

---
+++ Git Test/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Git Test
+//
+//  Created by Yitao Zhang on 4/10/15.
+//  Copyright (c) 2015 Yitao Zhang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Git Test/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Git Test
+//
+//  Created by Yitao Zhang on 4/10/15.
+//  Copyright (c) 2015 Yitao Zhang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
